c99b670ce74acdc4b7c372f7c0ce86a30f3d534a
Roni <rtlockwoodwork@gmail.com>
2022-05-22 13:52:18 -0600

    added fade for home

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -4,19 +4,23 @@ import 'react-alice-carousel/lib/alice-carousel.css';
 
 
 
-const Carousel = ({blok})  => {
+
+const Carousel = ({blok})  => { 
+  
   return (
     
    <div className="  h-full w-full md:h-3/4 md:w-3/4 m-auto">
-     <AliceCarousel autoPlay autoPlayInterval="3400"  autoHeight infinite>
-        {blok.block.map((blok) => (
+     <AliceCarousel autoPlay autoPlayInterval="3400" infinite animationType="fadeout" animationDuration={800}>
+     {blok.block.map((blok) => (
      
-          <img key={blok._uid} className="object-cover max-h-80 w-3/4 m-auto" src={blok.img} id={blok._uid} alt={blok.title} width={600} height={300} />
-     
-        ))}
+     <img key={blok._uid} className="object-cover max-h-80 w-3/4 m-auto" src={blok.img} id={blok._uid} alt={blok.title}  width={600} height={300} />
+
+   ))}
       </AliceCarousel>
    </div>
     
   )
 }
-export default Carousel
+
+
+export default  Carousel
